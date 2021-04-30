7534ead6f608e2405d07d589869660aeb18a8fb4
kmiyamoto-eng <muryo40@gmail.com>
2021-05-01 01:50:10 +0900

    一覧表示機能 index を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
 
-def new
-  @user = User.new
-end
-
-def create
-  User.create(user_params)
-end
-
-private
-  def user_params
-    params.require(:user).permit(:name, :age)
+  def index
+    @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :age)
+    end
 end
