a27039f6b776a30f2852c59031b6c550fd3de87b
motoki <81569237+motokiteruya@users.noreply.github.com>
2021-05-04 23:40:21 +0900

    コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -1,18 +1,7 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_110314) do
+ActiveRecord::Schema.define(version: 2021_04_30_151921) do
 
-  # These are extensions that must be enabled in order to support this database
+  
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
@@ -22,9 +11,16 @@ ActiveRecord::Schema.define(version: 2021_04_26_110314) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "message", force: :cascade do |t|
+  create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.integer "contents"
+    t.text "contents"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
